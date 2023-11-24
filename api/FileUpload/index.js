83ac773a07208c67c3818110ee4650d113c81a84
Vlad Voskoboynikov <vladv@vladv.org>
2023-11-23 22:35:35 -0600

    Fix CosmosDB

--- api/FileUpload/index.js
+++ api/FileUpload/index.js
@@ -3,6 +3,7 @@ const {
   StorageSharedKeyCredential,
   newPipeline
 } = require("@azure/storage-blob");
+const { CosmosClient } = require("@azure/cosmos");
 
 const streamifier = require('streamifier');
 const multipart = require('parse-multipart');
@@ -11,6 +12,9 @@ const STORAGE_ACCOUNT = process.env.STORAGE_ACCOUNT;
 const STORAGE_KEY = process.env.STORAGE_KEY;
 const STORAGE_CONTAINER = process.env.STORAGE_CONTAINER;
 const STORAGE_URL = `https://${STORAGE_ACCOUNT}.blob.core.windows.net`;
+
+const COSMOS_DB_CONNECTION_STRING = process.env.COSMOS_DB_CONNECTION_STRING;
+const COSMOS_DB_DATABASE_NAME = process.env.COSMOS_DB_DATABASE_NAME;
 
 module.exports = async function (context, req) {
   // Get the image data from the request
@@ -48,12 +52,25 @@ module.exports = async function (context, req) {
     }
   );
 
-  context.bindings.cosmos = {
+  const item= {
     id: fileName,
     imageUrl: `${STORAGE_URL}/${STORAGE_CONTAINER}/${fileName}`
   }
 
+  const cosmosClient = new CosmosClient(COSMOS_DB_CONNECTION_STRING);
+  const { database } = await cosmosClient.databases.createIfNotExists({ id: COSMOS_DB_DATABASE_NAME });
+  const { container } = await database.containers.createIfNotExists({
+    id: "vvdotcr-fileupload-dev",
+    partitionKey: {
+        paths: "/id"
+    }
+  });
+  const { resource } = await container.items.create(item);
+  
   context.res = {
-    body: { imageUrl: `${STORAGE_URL}/${STORAGE_CONTAINER}/${fileName}` }
+    body: {
+      imageUrl: `${STORAGE_URL}/${STORAGE_CONTAINER}/${fileName}`,
+      cosmosResource: resource
+    }
   };
 };
