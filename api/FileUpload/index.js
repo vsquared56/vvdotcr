703fc8299ea729082fef0fe65b3fc42922f31db1
Vlad Voskoboynikov <vladv@vladv.org>
2023-12-04 18:19:51 -0600

    Use azure-functions-multipart

--- api/FileUpload/index.js
+++ api/FileUpload/index.js
@@ -4,10 +4,9 @@ const {
   newPipeline
 } = require("@azure/storage-blob");
 const { CosmosClient } = require("@azure/cosmos");
-
+const parseMultipartFormData = require("@anzp/azure-function-multipart").default;
 const crypto = require('crypto');
 const streamifier = require('streamifier');
-const multipart = require('parse-multipart');
 
 const STORAGE_ACCOUNT = process.env.STORAGE_ACCOUNT;
 const STORAGE_KEY = process.env.STORAGE_KEY;
@@ -21,65 +20,73 @@ module.exports = async function (context, req) {
   // Get the image data from the request
   const bodyBuffer = Buffer.from(req.body);
 
-  // use the parse-multipart library to parse the multipart form data
-  const boundary = multipart.getBoundary(req.headers["content-type"]);
-  const parts = multipart.Parse(bodyBuffer, boundary);
-
-  const fileData = parts[0].data;
-  // Append a date string to the front to make every file name unique
-  const originalFileName =  parts[0].filename;
-  const fileName = crypto.randomUUID();
-  const contentType = parts[0].type;
-
-  // Set auth credentials for upload
-  const sharedKeyCredential = new StorageSharedKeyCredential(
-    STORAGE_ACCOUNT,
-    STORAGE_KEY
-  );
-  const pipeline = newPipeline(sharedKeyCredential);
-
-  // Upload the file
-  const blobServiceClient = new BlobServiceClient(STORAGE_URL, pipeline);
-  const containerClient =
-    blobServiceClient.getContainerClient(STORAGE_CONTAINER);
-  const blockBlobClient = containerClient.getBlockBlobClient(fileName);
-  const uploadBlobResponse = await blockBlobClient.uploadStream(
-    streamifier.createReadStream(new Buffer(fileData)),
-    fileData.length,
-    5,
-    {
-      blobHTTPHeaders: {
-        blobContentType: contentType
+  const { fields, files } = await parseMultipartFormData(req);
+  if (files.length != 1) {
+    context.res = {
+      status: 400,
+      body: {
+        error: "Expected only one file upload."
       }
-    }
-  );
-  
-  const createDate = Date.now();
-  const item= {
-    id: fileName,
-    originalFileName: originalFileName,
-    originalFileType: contentType,
-    uploadUserAgent: req.headers['user-agent'],
-    uploadXFF: req.headers['x-forwarded-for'],
-    createDate: createDate,
-    modifyDate: createDate,
-    imageUrl: `${STORAGE_URL}/${STORAGE_CONTAINER}/${fileName}`
+    };
   }
+  else {
 
-  const cosmosClient = new CosmosClient(COSMOS_DB_CONNECTION_STRING);
-  const { database } = await cosmosClient.databases.createIfNotExists({ id: COSMOS_DB_DATABASE_NAME });
-  const { container } = await database.containers.createIfNotExists({
-    id: "vvdotcr-fileupload-dev",
-    partitionKey: {
+    const fileData = files[0].bufferFile;
+    // Append a date string to the front to make every file name unique
+    const originalFileName = files[0].filename;
+    const fileName = crypto.randomUUID();
+    const contentType = files[0].mimetype;
+
+    // Set auth credentials for upload
+    const sharedKeyCredential = new StorageSharedKeyCredential(
+      STORAGE_ACCOUNT,
+      STORAGE_KEY
+    );
+    const pipeline = newPipeline(sharedKeyCredential);
+
+    // Upload the file
+    const blobServiceClient = new BlobServiceClient(STORAGE_URL, pipeline);
+    const containerClient =
+      blobServiceClient.getContainerClient(STORAGE_CONTAINER);
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+    const uploadBlobResponse = await blockBlobClient.uploadStream(
+      streamifier.createReadStream(new Buffer(fileData)),
+      fileData.length,
+      5,
+      {
+        blobHTTPHeaders: {
+          blobContentType: contentType
+        }
+      }
+    );
+
+    const createDate = Date.now();
+    const item = {
+      id: fileName,
+      originalFileName: originalFileName,
+      originalFileType: contentType,
+      uploadUserAgent: req.headers['user-agent'],
+      uploadXFF: req.headers['x-forwarded-for'],
+      createDate: createDate,
+      modifyDate: createDate,
+      imageUrl: `${STORAGE_URL}/${STORAGE_CONTAINER}/${fileName}`
+    }
+
+    const cosmosClient = new CosmosClient(COSMOS_DB_CONNECTION_STRING);
+    const { database } = await cosmosClient.databases.createIfNotExists({ id: COSMOS_DB_DATABASE_NAME });
+    const { container } = await database.containers.createIfNotExists({
+      id: "vvdotcr-fileupload-dev",
+      partitionKey: {
         paths: "/id"
-    }
-  });
-  const { resource } = await container.items.create(item);
-  
-  context.res = {
-    body: {
-      imageUrl: `${STORAGE_URL}/${STORAGE_CONTAINER}/${fileName}`,
-      cosmosResource: resource
-    }
-  };
+      }
+    });
+    const { resource } = await container.items.create(item);
+
+    context.res = {
+      body: {
+        imageUrl: `${STORAGE_URL}/${STORAGE_CONTAINER}/${fileName}`,
+        cosmosResource: resource
+      }
+    };
+  }
 };
