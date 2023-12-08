823e0719143f9593772cf4efbd6bfc04b846927c
Vlad Voskoboynikov <vladv@vladv.org>
2023-12-07 22:51:23 -0600

    Print file content in current directory

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -4,23 +4,13 @@ module.exports = async function (context, req) {
   //joining path of directory 
   const directoryPath = context.executionContext.functionDirectory;
 
-  var allFiles;
-  //passsing directoryPath and callback function
-  fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-      allFiles = ('Unable to scan directory: ' + err);
-    }
-    else {
-      allFiles = files;
-    }
-  });
+  const fileContent = fs.readFileSync(context.executionContext.functionDirectory + '\\sample.dat')
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: {
       text: "This data is returned from the API!",
       request: req,
-      allFiles: allFiles,
+      fileContent: fileContent,
       directoryPath: directoryPath
     },
   };
