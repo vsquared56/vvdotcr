6a7ac83f01bdb82bc6961100788032a2e48f88ce
Vlad Voskoboynikov <vladv@vladv.org>
2023-12-17 21:55:18 -0600

    Add xff parsing to GetMessage

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const path = require("path");
 const handlebars = require("handlebars");
+const { CosmosClient } = require("@azure/cosmos");
+const ipRangeCheck = require("ip-range-check");
+
+const COSMOS_DB_CONNECTION_STRING = process.env.COSMOS_DB_CONNECTION_STRING;
+const COSMOS_DB_DATABASE_NAME = process.env.COSMOS_DB_DATABASE_NAME;
 
 module.exports = async function (context, req) {
   const directoryPath = path.join(context.executionContext.functionDirectory, '..', 'views', 'sample.hbs');
@@ -8,12 +13,55 @@ module.exports = async function (context, req) {
   const templateContent = fs.readFileSync(directoryPath).toString();
   var template = handlebars.compile(templateContent);
 
+  var clientIp = null;
+  if (req.headers.hasOwnProperty("x-forwarded-for")) {
+    var trustedIpBlocks;
+    const cosmosClient = new CosmosClient(COSMOS_DB_CONNECTION_STRING);
+    const { database } = await cosmosClient.databases.createIfNotExists({ id: COSMOS_DB_DATABASE_NAME });
+    const { container } = await database.containers.createIfNotExists({
+      id: "vvdotcr-settings-dev",
+      partitionKey: {
+        paths: "/id"
+      }
+    });
+    const { resource } = await container.item("trusted_xff_ip_blocks", "trusted_xff_ip_blocks").read();
+    if (resource === undefined || !resource.hasOwnProperty('blocks') || resource.length == 0) {
+      throw new Error('Error reading trusted IP blocks from CosmosDB');
+    }
+    else {
+      trustedIpBlocks = resource.blocks;
+    }
+
+    const regex = /^(.*?(?:[^:]|::))(?:(:)([0-9]*))?$/;
+    const xff = "67.173.22.116:42104, 13.89.171.4:3475";
+    const xffArr = xff.replace(/\s/g, "").split(",")
+    var clientIp = null;
+    i = xffArr.length - 1;
+    do {
+      const matches = xffArr[i].match(regex);
+      if (matches && matches.length == 4) {
+        const ip = matches[1].replace(/\[|\]/g, "")
+        if (ipRangeCheck(ip, trustedIpBlocks)) {
+          i--;
+        }
+        else {
+          clientIp = ip;
+        }
+      }
+      else {
+        throw new Error('Error parsing IP from X-Forwarded-For: ' + xffArr[i]);
+      }
+    }
+    while (i >= 0 && clientIp == null);
+  }
+
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: {
       text: template({ name: "Vlad" }),
       directoryPath: directoryPath,
-      request: req
+      request: req,
+      clientIp: clientIp
     },
   };
 };
