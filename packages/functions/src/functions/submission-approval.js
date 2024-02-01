7e11583eced26f06e86c1ee40510c8e01b5ba3b6
Vlad Voskoboynikov <vladv@vladv.org>
2024-01-31 23:01:34 -0600

    Add geolocking

--- packages/functions/src/functions/submission-approval.js
+++ packages/functions/src/functions/submission-approval.js
@@ -3,6 +3,8 @@ import { ServiceBusClient } from "@azure/service-bus";
 import exifr from "exifr";
 import fetch from 'node-fetch';
 import sharp from "sharp";
+import circle from '@turf/circle';
+import booleanIntersects from '@turf/boolean-intersects';
 
 import * as utils from "@vvdotcr/common";
 
@@ -13,20 +15,39 @@ app.serviceBusQueue('submission-approval', {
     queueName: 'new-sightings-to-validate',
     handler: async (message, context) => {
         const item = await utils.getSighting(message);
+        const minLocationAccuracy = await utils.getSetting("min_location_accuracy_meters");
 
         if (item.imageLocation === null) {
             item.submissionStatus = 'needsManualApproval';
             item.automaticApprovalDenied = 'missingLocation';
-        } else if (item.imageLocation.source === 'browser' && item.imageLocation.accuracy > 250) {
+        } else if (item.imageLocation.source === 'browser' && item.imageLocation.accuracy > minLocationAccuracy) {
             item.submissionStatus = 'needsManualApproval';
             item.automaticApprovalDenied = 'inaccurateBrowserLocation';
         }
         else {
-            item.submissionStatus = 'approved';
-            item.publishDate = Date.now();
-            item.publishedBy = "automaticApproval";
+            const geolockedLocations = await utils.getSetting("geolocked_locations");
+            const center = [item.imageLocation.longitude, item.imageLocation.latitude];
+            const radius = item.imageLocation.accuracy;
+            const options = { steps: 20, units: 'meters' };
+            const imageCircle = circle(center, radius, options);
+            var isGeolocked = false;
+
+            for (const geolock of geolockedLocations.features) {
+                if (booleanIntersects(geolock, imageCircle)) {
+                    isGeolocked = true;
+                }
+            }
+
+            if (isGeolocked) {
+                item.submissionStatus = 'needsManualApproval';
+                item.automaticApprovalDenied = 'geolocked';
+            } else {
+                item.submissionStatus = 'approved';
+                item.publishDate = Date.now();
+                item.publishedBy = "automaticApproval";
+            }
         }
-        
+
         await utils.saveSighting(item);
     },
 });
