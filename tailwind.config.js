4386bc306278d491f4d28fa3a871dffa73593b69
Vlad Voskoboynikov <vladv@vladv.org>
2024-02-05 23:12:12 -0600

    Add tailwind, with some janky 'build' scripts

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./packages/swa/src/*.{html,css}","./packages/swa/api/views/*.hbs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
