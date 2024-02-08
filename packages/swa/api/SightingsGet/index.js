853e1f49849e024a0229aa5f8017ebf6d1418ebb
Vlad Voskoboynikov <vladv@vladv.org>
2024-02-07 18:17:29 -0600

    Display sighting dates instead of ugly IDs

--- packages/swa/api/SightingsGet/index.js
+++ packages/swa/api/SightingsGet/index.js
@@ -2,6 +2,13 @@ import * as utils from "@vvdotcr/common";
 
 export default async (context, req) => {
   var response = "";
+
+  const dateOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
   const page = req.query.page ? parseInt(req.query.page) : 0;
   const sightings = await utils.getPaginatedSightings(2, false, page);
 
@@ -16,11 +23,11 @@ export default async (context, req) => {
     var itemCount = 1;
     for (const sighting of sightings.items) {
       response += utils.renderTemplate(
-        'sightings_item',
+        'sightings_card',
         {
           sightingId: sighting.id,
           sightingImage: sighting.thumbnailImageUrl,
-          sightingDate: sighting.createDate,
+          sightingDate: new Date(sighting.createDate).toLocaleDateString('en-US', dateOptions),
           loadMore: (itemCount === sightings.items.length && sightings.continuationToken !== null),
           nextPage: page + 1
         },
