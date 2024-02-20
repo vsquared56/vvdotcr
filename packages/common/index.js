9a222846c921d6378b550e5d468422877044a9c5
Vlad Voskoboynikov <vladv@vladv.org>
2024-02-19 23:56:40 -0600

    Remove old handlebars code

--- packages/common/index.js
+++ packages/common/index.js
@@ -1,5 +1,4 @@
 export { Database } from './db.js';
-export { renderTemplate } from './template.js';
 export { renderSightingProperty } from './sighting_properties.js';
 export { Storage } from './storage.js';
 export { parseXff } from './xff.js';
