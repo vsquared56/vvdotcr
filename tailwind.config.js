8b4c90b0b9e832a7fe602c4c7a29253751b758bd
Vlad Voskoboynikov <vladv@vladv.org>
2024-03-07 19:05:33 -0600

    Adjust modal backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./packages/swa/src/*.{html,css}","./packages/swa/api/views/**"],
   theme: {
     extend: {},
+    colors: {
+      'transparent-black': {
+        100: '#00000010',
+        200: '#00000030',
+        300: '#00000050',
+        400: '#00000070',
+        500: '#00000080',
+        600: '#000000a0',
+        700: '#000000c0',
+        800: '#000000e0',
+        900: '#000000ff',
+      },
+    }
   },
   plugins: [require("daisyui")],
   daisyui: {
