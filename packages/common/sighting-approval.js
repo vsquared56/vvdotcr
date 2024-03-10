fe69e738c39c530b38d93ba3e64f436762896dbd
Vlad Voskoboynikov <vladv@vladv.org>
2024-03-09 21:42:28 -0600

    Score sightings based on Azure vision tags

--- packages/common/sighting-approval.js
+++ packages/common/sighting-approval.js
@@ -5,6 +5,21 @@ export async function sightingApproval(db, sightingId, sendNotifications) {
   const serviceBusConnectionString = process.env.SERVICE_BUS_CONNECTION_STRING;
   const sighting = await db.getSighting(sightingId);
   const minLocationAccuracy = await db.getSetting("min_location_accuracy_meters");
+  const tagScores = await db.getSetting("sighting_tag_scores");
+
+  var score = 0;
+  for (const tag of sighting.visionData.tagsResult.values) {
+    console.log(`Tag ${tag.name} Confidence: ${tag.confidence}`);
+    if (tagScores.tags[tag.name]) {
+      if ((tagScores.tags[tag.name].minConfidence && tag.confidence >= tagScores.tags[tag.name].minConfidence) ||
+          (!tagScores.tags[tag.name].minConfidence && tag.confidence >= tagScores.defaultMinConfidence)) {
+        console.log(tagScores.tags[tag.name]);
+        score += (tagScores.tags[tag.name].score * tag.confidence);
+      }
+    }
+  }
+  sighting.sightingScore = score;
+  console.log(`Final score ${score}`);
 
   if (sighting.imageLocation === null || sighting.imageLocation.latitude === null || sighting.imageLocation.longitude === null) {
     sighting.submissionStatus = 'needsManualApproval';
@@ -20,8 +35,10 @@ export async function sightingApproval(db, sightingId, sendNotifications) {
     if (isGeolocked) {
       sighting.submissionStatus = 'needsManualApproval';
       sighting.automaticApprovalDenied = 'geolocked';
+    } else if (score < (await db.getSetting("min_sighting_score"))) {
+      sighting.submissionStatus = 'needsManualApproval';
+      sighting.automaticApprovalDenied = 'lowScore';
     } else {
-      sighting.sightingScore = 1;
       sighting.submissionStatus = 'approved';
       sighting.isPublished = true;
       sighting.publishDate = Date.now();
