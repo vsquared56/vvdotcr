9dc15148bb4ca47ff2d41c796b4e957acf85c894
Vlad Voskoboynikov <vladv@vladv.org>
2024-03-09 22:28:03 -0600

    Ping the SWA API functions from the backend functions to improve cold start times

--- packages/functions/src/functions/cold-start-timer.js
+++ packages/functions/src/functions/cold-start-timer.js
@@ -1,8 +1,14 @@
 import { app } from '@azure/functions';
 
+const coldStartPingUrl = process.env.PING_URL;
+
 app.timer('cold-start-timer', {
-    schedule: '*/3 * * * *',
-    handler: async (myTimer, context) => {
-        const run = "Triggered."
-    }
+  schedule: '*/3 * * * *',
+  handler: async (myTimer, context) => {
+    await fetch(coldStartPingUrl, {
+      headers: {
+        "User-Agent": "cold-start-timer"
+      }
+    });
+  }
 });
