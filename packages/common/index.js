04d28abc067a07ea38e7b62a3f0033b71171bb1c
Vlad Voskoboynikov <vladv@vladv.org>
2024-03-10 00:21:24 -0600

    Standardize js file name

--- packages/common/index.js
+++ packages/common/index.js
@@ -3,7 +3,7 @@ export { processFormItems } from './form.js'
 export { parseLocationForm, isLocationInFeatureCollection } from './location.js'
 export { getOrCreateSession, getSession, getResponseCookie, isActionRateLimited, saveAction } from './session-limits.js'
 export { sightingApproval } from './sighting-approval.js';
-export { renderSightingProperty } from './sighting_properties.js';
+export { renderSightingProperty } from './sighting-properties.js';
 export { Storage } from './storage.js';
 export { validateTurnstileResponse } from './turnstile.js';
 export { parseXff } from './xff.js';
