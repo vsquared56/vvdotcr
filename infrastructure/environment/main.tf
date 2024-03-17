fd497a876f4e4c2799dacabf4387a0971f5114b9
Vlad Voskoboynikov <vladv@vladv.org>
2024-03-16 22:04:27 -0500

    Add TF remote state

--- infrastructure/environment/main.tf
+++ infrastructure/environment/main.tf
@@ -26,3 +26,15 @@ resource "azurerm_resource_group" "environment_rg" {
   location = "centralus"
   name     = "vvdotcr-${local.environment}"
 }
+
+//Remote state for resources shared between different environments
+data "terraform_remote_state" "shared_rg" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = "vvdotcr"
+    storage_account_name = "vvdotcrcommon"
+    container_name       = "tfstate"
+    key                  = "terraform-shared.tfstate"
+  }
+}
