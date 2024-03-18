fe0bd0b08bd35788455f14dc26b8dcfd137de9c4
Vlad Voskoboynikov <vladv@vladv.org>
2024-03-17 20:08:41 -0500

    Add CDN DNS record to blob storage

--- infrastructure/environment/app-storage.tf
+++ infrastructure/environment/app-storage.tf
@@ -15,6 +15,15 @@ resource "azurerm_storage_account" "app_storage" {
     tags = {}
 }
 
+resource "cloudflare_record" "cdn_cname" {
+  zone_id = data.terraform_remote_state.shared_rg.outputs.cloudflare_zone_id
+  name    = "cdn-dev"
+  value   = azurerm_storage_account.app_storage.primary_blob_host
+  type    = "CNAME"
+  proxied = true
+  ttl     = 1
+}
+
 resource "azurerm_storage_container" "sightings_private" {
   name                  = "vvdotcr-sightings-private-${local.environment}"
   storage_account_name  = azurerm_storage_account.app_storage.name
