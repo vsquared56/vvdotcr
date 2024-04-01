27642693a363881fa8e7db3f3f9a67df7aba94f5
Vlad Voskoboynikov <vladv@vladv.org>
2024-03-31 21:32:13 -0500

    Use event-driven modal rendering

--- packages/swa/src/index.js
+++ packages/swa/src/index.js
@@ -1,4 +1,5 @@
 import 'htmx.org';
 import './global_htmx.js';
 import './htmx_error.js';
+import './modal.js';
 import './stars.js';
--- packages/swa/src/modal.js
+++ packages/swa/src/modal.js
@@ -1,65 +1,66 @@
-function renderMessageModal(event) {
-  if (event.target.id==='message-submit') {
+document.addEventListener("modalRender", renderModal);
+
+function renderModal(event) {
+  if (event.detail.modalId === "message-submit" && event.detail.sourceEvent.target.id === "message-submit") {
     turnstile.render(
-      '#turnstile-message',
+      "#turnstile-message",
       {
-        sitekey: htmx.find('#turnstile-site-key').value,
-        theme: 'dark',
+        sitekey: htmx.find("#turnstile-site-key").value,
+        theme: "dark",
         callback: messageTurnstileCallback,
-        'expired-callback': messageTurnstileExpiredCallback
+        "expired-callback": messageTurnstileExpiredCallback
       }
     );
-    htmx.find('#message-submit').showModal();
-    htmx.find('#message-submit').focus();
-  }
-}
-
-function messageTurnstileCallback(token) {
-  var button = document.getElementById('message-send-button');
-  button.disabled = false;
-  button.innerHTML = "Send!";
-}
-
-function messageTurnstileExpiredCallback() {
-  var button = document.getElementById('message-send-button');
-  button.disabled = true;
-  button.innerHTML = "Please verify you're not a bot";
-}
-
-function renderSightingModal(event) {
-  if (event.target.id==='sighting-submit') {
+    htmx.find("#message-submit").showModal();
+    htmx.find("#message-submit").focus();
+  } else if (event.detail.modalId === "sighting-submit" && event.detail.sourceEvent.target.id === "sighting-submit") {
     turnstile.render(
-      '#turnstile-sighting',
+      "#turnstile-sighting",
       {
-        sitekey: htmx.find('#turnstile-site-key').value,
-        theme: 'dark',
+        sitekey: htmx.find("#turnstile-site-key").value,
+        theme: "dark",
         callback: sightingUpdateSubmit,
-        'expired-callback': sightingUpdateSubmit
+        "expired-callback": sightingUpdateSubmit
       }
     );
     const fileInput = document.getElementById("sighting-file-input");
     fileInput.addEventListener("change", sightingFileChange);
-    htmx.find('#sighting-submit').showModal();
-    htmx.find('#sighting-submit').focus();
+    htmx.find("#sighting-submit").showModal();
+    htmx.find("#sighting-submit").focus();
 
-    htmx.on('#form', 'htmx:xhr:progress', function (evt) {
+    htmx.on("#form", "htmx:xhr:progress", function (evt) {
       //Clicking submit also fires off a GET request to Cloudflare Turnstile
       //Prevent any actions on the progress of that request by checking if it's lengthComputable
       if (evt.detail.lengthComputable) {
-        htmx.find('#progress-upload').setAttribute('value', evt.detail.loaded / evt.detail.total * 100);
+        htmx.find("#progress-upload").setAttribute("value", evt.detail.loaded / evt.detail.total * 100);
         if (evt.detail.loaded == evt.detail.total) {
           htmx.remove(htmx.find("#progress-upload"));
           var waitSpan = document.createElement("span");
           waitSpan.classList.add("loading","loading-dots","loading-lg","shrink");
           htmx.find("#submit-progress").appendChild(waitSpan);
-          htmx.off('#form', 'htmx:xhr:progress', this);
+          htmx.off("#form", "htmx:xhr:progress", this);
         }
       }
     });
-    htmx.on('#form', 'htmx:confirm', function (evt) {
-      htmx.find('#sighting-send-button').setAttribute('disabled', true);
+    htmx.on("#form", "htmx:confirm", function (evt) {
+      htmx.find("#sighting-send-button").setAttribute("disabled", true);
     });
+  } else {
+    htmx.find(`#${event.detail.modalId}`).showModal();
+    htmx.find(`#${event.detail.modalId}`).focus();
   }
+}
+
+function messageTurnstileCallback(token) {
+  var button = document.getElementById("message-send-button");
+  button.disabled = false;
+  button.innerHTML = "Send!";
+}
+
+function messageTurnstileExpiredCallback() {
+  var button = document.getElementById("message-send-button");
+  button.disabled = true;
+  button.innerHTML = "Please verify you're not a bot";
 }
 
 function sightingFileChange() {
